6c54b94dadf8528c7131985dacaf222d5f56196f
Terry Fadz <tfadz@suntimes.com>
2018-11-29 16:47:23 -0600

    updated formatting

--- amp-gtm-instructions.php
+++ amp-gtm-instructions.php
@@ -6,9 +6,13 @@ In Google Tag Manager..
 
 2. Add a Trigger and use Page View for type.
 
-3. Create new AMP variables that link to Wordpress dimensions (author, postid, tags,etc). Create new AMP variable and make sure the actual variable names starts with "dimension1" (this connects it to the custom dimension created in the GA account).
+3. Create new AMP variables that link to Wordpress dimensions (author, postid, tags,etc). 
+Create new AMP variable and make sure the actual variable names starts with "dimension1" 
+(this connects it to the custom dimension created in the GA account).
 
-4. Go back to "Tags" and select your GA account. Create Custom Dimensions starting with numeral 1 as the index and then connecting it to the variales you setup before. Trigger All Pages. (In our code below we put in inactive dimensions for dimension3-5).
+4. Go back to "Tags" and select your GA account. Create Custom Dimensions starting with 
+numeral 1 as the index and then connecting it to the variales you setup before. 
+Trigger All Pages. (In our code below we put in inactive dimensions for dimension3-5).
 
 5. In your AMP file make sure the markup looks something like this.
 
