5c82cd018bd5007f72ecfdf17660e3bdb44f8049
Terry Fadz <terry@glantz.net>
2019-09-20 11:49:49 -0500

    Added posts by tag

--- get-posts.php
+++ get-posts.php
@@ -32,3 +32,26 @@ $obj = get_post_type_object( 'case_study' ); ?>
 <div class="cs-header__title h5"><?php echo $obj->labels->singular_name; ?></div>
 <h1><?php the_title() ?></h1>
 <div><?php the_category(); ?></div>
+
+
+<?php
+// Pull Posts
+$args = array(
+  'posts_per_page' => -1,
+  'no_found_rows'  => true,
+  'tag'            => 'bicentenary-2019' 
+);
+$loop = new WP_Query($args);
+if( $loop->have_posts() ) : ?>
+  <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+    <article>
+      <a href="<?php the_permalink(); ?>">
+        <figure><?php the_post_thumbnail(); ?></figure></a>
+        <h2><?php the_title() ?></h2>
+      </article>
+    <?php endwhile; ?> 
+  <?php endif;
+  wp_reset_postdata();
+  ?>
+
+
